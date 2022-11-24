8255eea428f4fa759f6498c079d9101c1866e5ed
Isai Perez <72577853+alfredo2201@users.noreply.github.com>
2022-11-23 22:21:41 -0700

    noPage fix

--- front/src/pages/NoPage.jsx
+++ front/src/pages/NoPage.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-const Home = () => {
+const NoPage = () => {
   return (
     <div>
         <h1>No hay pagina</h1>
@@ -8,4 +8,4 @@ const Home = () => {
   );
 };
 
-export default Home
+export default NoPage
